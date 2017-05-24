97065b7b5a22b86a92e94169f5deda6091644de9
rhdzmota <rhdzmota@mxquants.com>
2017-05-23 23:10:23 -0500

    add init file

---
+++ __init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 23:05:30 2017
+
+@author: rhdzmota
+"""
+
+# %% define imports
+
+import data
+
+# %% 
+print("\nCongrats, you are using a mxquant package.\n")
+
+# %% main function 
+def main():
+    return True 
+
+# %% 
+if __name__ == "__main__":
+    main()
+    
