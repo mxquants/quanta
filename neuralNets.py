ca1cd491e18ac0918e63b476b1a6c5a84468c11e
rhdzmota <rhdzmota@mxquants.com>
2017-06-04 23:17:44 -0500

    add MLP wrapper for tensorflow

---
+++ neuralNets.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Neural Networks
+Simple neural networks using tensorflow
+
+@author: rhdzmota
+"""
+
+# Imports
+import numpy as np
+import tensorflow as tf
+
+
+class mlpRegressor(object):
+    """MultiLayer Perceptron Regressor using tensorflow."""
+
+    def __init__(self, hidden_layers=[1], activation_function="sigmoid"):
+        """Initialize mlp Regressor."""
+        self.hidden_layers = list(hidden_layers)
+        self.activation = self._activation(activation_function)
+
+    def _activation(self, _string):
+        functions = {"sigmoid": tf.nn.sigmoid,
+                     "relu": tf.nn.relu,
+                     "softmax": tf.nn.softmax}
+        return functions.get(_string)
+
+    def _initilizeModel(self, n_inputs, n_outputs=1):
+        self.weights, self.bias = {}, {}
+        _c = 0
+        last_size = n_inputs
+        # no hidden layers
+        if not len(self.hidden_layers):
+            self.weights[_c] = tf.Variable(tf.zeros([last_size, n_outputs]))
+            self.bias[_c] = tf.Variable(tf.zeros([n_outputs]))
+        # at least 1 hidden layer
+        else:
+            for i in self.hidden_layers+[n_outputs]:
+                self.weights[_c] = tf.Variable(tf.zeros([last_size, i]))
+                self.bias[_c] = tf.Variable(tf.zeros([i]))
+                last_size = i  # change "columns" to "rows" for next layer.
+                _c += 1  # increase position
+        return self.weights, self.bias
+
+    def prediction(self, x, weights, biases):
+        """."""
+        layers = [x]
+        for i in sorted(weights.keys()):
+            layers.append(tf.nn.sigmoid(tf.add(tf.matmul(
+                                                         layers[-1],
+                                                         weights[i]),
+                                               biases[i])))
+        self.layers = layers
+        return layers[-1]
+
+    def cost(self, pred, y):
+        """Cost function."""
+        return tf.reduce_mean(tf.squared_difference(pred, y))
+
+    def train(self, dataset, alpha=0.01, epochs=500):
+        """Train the model."""
+        self.n_inputs = np.shape(dataset.input_data)[-1]
+        self.n_outputs = np.shape(dataset.output_data)[-1]
+        y = tf.placeholder(tf.float32, [None, self.n_outputs])
+        x = tf.placeholder(tf.float32, [None, self.n_inputs])
+
+        tf_vars = {}
+        self.weights, self.biases = self._initilizeModel(self.n_inputs,
+                                                         self.n_outputs)
+        tf_vars["pred"] = self.prediction(x, self.weights, self.biases)
+        tf_vars["cost"] = self.cost(tf_vars["pred"], y)
+        tf_vars["opt"] = tf.train.AdamOptimizer(alpha).minimize(
+                                                    tf_vars["cost"])
+        tf_vars["mse"] = tf.squared_difference(tf_vars["pred"], y)
+        self.epoch_error = []
+        self.init_op = tf.global_variables_initializer()
+        result = {"y": [np.asscalar(i) for i in dataset.train[-1]]}
+        with tf.Session() as sess:
+            sess.run(self.init_op)
+            for epoch in range(epochs):
+                x_batch, y_batch = dataset.nextBatch()
+                sess.run(tf_vars["opt"], feed_dict={x: x_batch,
+                                                    y: y_batch})
+                self.epoch_error.append(np.mean(sess.run(tf_vars["mse"],
+                                        feed_dict={x: x_batch, y: y_batch})))
+            vals = sess.run(tf_vars["pred"], feed_dict={x: dataset.train[0]})
+            self.np_w = sess.run(self.weights)
+            self.np_b = sess.run(self.biases)
+            self.tf_vars = tf_vars
+        result["estimates"] = [np.asscalar(i) for i in vals]
+        result["errors"] = [j-i for i, j in zip(
+                                    result["estimates"], result["y"])]
+        result["square_error"] = [i**2 for i in result["errors"]]
+        self.train_results = result
+
+    def freeEval(self, free_input):
+        """Custom input to evaluate."""
+        x = tf.placeholder(tf.float32, [None, self.n_inputs])
+        weights = self.np_w
+        biases = self.np_b
+        pred = self.prediction(x, weights, biases)
+        self.init_op = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(self.init_op)
+            vals = sess.run(pred, feed_dict={x: free_input})
+        return vals
+
+    def evaluate(self, dataset):
+        """Evaluate train data from a given dataset."""
+        x_test = dataset.test[0]
+        return self.freeEval(x_test)
+
+    def test(self, dataset):
+        """Test data from a dataset object."""
+        vals = self.evaluate(dataset)
+        real_y = dataset.test[-1]
+        result = {"estimates": [np.asscalar(i) for i in vals],
+                  "y": [np.asscalar(i) for i in real_y]}
+        result["errors"] = [j-i for i, j in zip(
+                                    result["estimates"], result["y"])]
+        result["square_error"] = [i**2 for i in result["errors"]]
+        self.test_results = result
+        return result
